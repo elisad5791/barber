8f5b18b599bb672a0ade13866fc6ddd127c6673f
elisad5791 <elisad5791@mail.ru>
2025-08-03 20:07:57 +0500

    Add migrations

---
+++ database/migrations/2025_08_03_145655_create_timeslots_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('timeslots', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('start');
+            $table->timestamp('finish');
+            $table->foreignId('master_id')->constrained();
+            $table->foreignId('service_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('timeslots');
+    }
+};
---
+++ database/migrations/2025_08_03_145733_create_master_service_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('master_service', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('master_id')->constrained();
+            $table->foreignId('service_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('master_service');
+    }
+};
